6c8ee338105073c9cd59a19ed2db290209133c7f
luis <luis.fer160197@gmail.com>
2023-12-04 01:00:13 -0600

    Add basic calculator structure

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  return operator(a, b)
+}
+
+const op = document.querySelector('.display.operation');
+const input = document.querySelector('.display.input');
