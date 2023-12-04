6386669531344887ff62b65573ded06580d9489a
luis <luis.fer160197@gmail.com>
2023-12-04 04:36:33 -0600

    Finished first version without styles

--- calculator.js
+++ calculator.js
@@ -1,22 +1,85 @@
 function add(a, b) {
   return a + b;
 }
-
 function substract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
-
 function operate(a, b, operator) {
   return operator(a, b)
 }
 
-const op = document.querySelector('.display.operation');
-const input = document.querySelector('.display.input');
+// my stuff
+
+function updateInput(a = '', b = '', op = '') {
+  if (a !== '') input.dataset.a += a;
+  if (b !== '') input.dataset.b += b;
+  if (op !== '') input.dataset.op = op;
+  updateInputDisplay();
+}
+
+function clearInput() {
+  input.dataset.a = '';
+  input.dataset.b = '';
+  input.dataset.op = '';
+}
+
+function updateInputDisplay() {
+  input.value = input.dataset.a + getOperatorSymbol(input.dataset.op) + input.dataset.b;
+}
+
+function getOperatorSymbol(operator) {
+  if (operator === 'add') return '+';
+  else if (operator === 'substract') return '-';
+  else if (operator === 'multiply') return 'x';
+  else if (operator === 'divide') return '÷';
+  else return '';
+}
+
+function solve() {
+  let num1 = input.dataset.a;
+  let num2 = input.dataset.b;
+  let op = input.dataset.op;
+  if (num1 !== '' && num2 !== '' && op !== '') {
+    const result = operate(+num1, +num2, window[op]);
+    input.value = result;
+    clearInput()
+    input.dataset.a = result;
+  }
+}
+
+const input = document.querySelector('#input');
+const btnNumbers = document.querySelectorAll('button.num');
+const btnOperators = document.querySelectorAll('button.operator');
+const btnEqual = document.querySelector('#equal');
+const btnClear = document.querySelector('#clear');
+
+// let currentOp = [0, add, 0];
+
+btnClear.addEventListener('click', () => {
+  clearInput();
+  updateInputDisplay()
+})
+
+btnNumbers.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (input.dataset.op === '') updateInput(btn.textContent, '', '');
+    else updateInput('', btn.textContent, '');
+  })
+});
+
+btnOperators.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (input.dataset.a === '') updateInput('0', '', btn.id);;
+    updateInput('', '', btn.id);
+  })
+});
+
+btnEqual.addEventListener('click', () => {
+  solve();
+});
